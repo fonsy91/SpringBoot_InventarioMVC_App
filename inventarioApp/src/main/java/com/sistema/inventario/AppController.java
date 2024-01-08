583fe1f032c53485677a9fb518d5af4e650febbf
alfonso91 <alfonso91fp@gmail.com>
2024-01-08 20:45:54 +0100

    Agrego ruta en el controller para el acceso a la aplicacion

--- inventarioApp/src/main/java/com/sistema/inventario/AppController.java
+++ inventarioApp/src/main/java/com/sistema/inventario/AppController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AppController {
 	
 	//Cuando arrancas la aplicacion la primera pagina es el index
+	// URL: http://localhost:8081/
 
 	@GetMapping("")
 	public String verPaginaDeInicio() {
